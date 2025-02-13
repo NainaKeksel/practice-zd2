2eea97c0cc6268407f48e49ef4d637293ac252be
Your Name <malenkoer@yandex.ru>
2025-02-13 12:31:29 +0700

    chetire

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 Vue.component('note', {
     props: ['card', 'columnIndex'],
-    template: `   
-    `,
+    template: ` 
+      <div class="note-card" :class="{ locked: card.locked }">
+             <h3>{{ card.title }}</h3>
+             <ul>
+                 <li v-for="(item, index) in card.items" :key="index">
+                     <input
+                      type="checkbox"
+                      :checked="item.completed"
+                      />
+                     {{ item.text }}
+                </li>
+            </ul>
+         </div>
+         `,
     methods: {
     }
 });
@@ -28,12 +40,28 @@ new Vue({
                 alert('Вторая колонка уже заполнена.');
                 return;
             }
+             const newCard = {
+                title: prompt('Введите заголовок карточки:'),
+                items: Array.from({ length: 4 }, () => ({
+                    text: prompt('Введите пункт списка:'),
+                    completed: false
+                })),
+                index: this.columns[columnIndex].cards.length,
+                completedDate: null,
+                locked: false
+            };
         },
     },
     template: `
         <div id="app">
-             <div v-for="(column, columnIndex) in columns" :key="columnIndex" class="column">
+            <div v-for="(column, columnIndex) in columns" :key="columnIndex" class="column">
                 <h2>{{ column.title }}</h2>
+                <note-card 
+                    v-for="(card, cardIndex) in column.cards" 
+                    :key="cardIndex" 
+                    :card="card" 
+                    :column-index="columnIndex"
+                ></note-card>
             </div>
         </div>
     `
