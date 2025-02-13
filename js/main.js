5d6f33426b93e671e4c0c2f52c1283697e5895a1
Your Name <malenkoer@yandex.ru>
2025-02-13 12:22:24 +0700

    tree

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+Vue.component('note', {
+    props: ['card', 'columnIndex'],
+    template: `   
+    `,
+    methods: {
+    }
+});
 new Vue({
     el: '#app',
     data() {
@@ -12,10 +19,20 @@ new Vue({
         };
     },
     methods: {
+        addCard(columnIndex) {
+            if (columnIndex === 0 && this.columns[0].cards.length >= this.maxCardsInColumnOne) {
+                alert('Первая колонка уже заполнена.');
+                return;
+            }
+            if (columnIndex === 1 && this.columns[1].cards.length >= this.maxCardsInColumnTwo) {
+                alert('Вторая колонка уже заполнена.');
+                return;
+            }
+        },
     },
     template: `
         <div id="app">
-             <div v-for="column in columns" class="column">
+             <div v-for="(column, columnIndex) in columns" :key="columnIndex" class="column">
                 <h2>{{ column.title }}</h2>
             </div>
         </div>
